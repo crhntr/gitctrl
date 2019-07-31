e712d9f9d226c1f5f93f06bc7227b2f95e766d4e
Christopher Hunter <chunter@pivotal.io>
2019-07-31 12:03:47 -0700

    start on multi branch file edit

--- main.go
+++ main.go
@@ -11,10 +11,17 @@ func main() {
 		wd = flag.Arg(1)
 	}
 
+	var args []string
+	if a := flag.Args(); len(a) > 1 {
+		args = a[1:]
+	}
+
 	switch command {
 	case "statuses":
 		statuses(wd)
 	case "remote-origin-must-ssh":
 		remoteOriginMustSSH(wd)
+	case "multi-branch-edit", "mbe":
+		multibranchedit(wd, args)
 	}
 }
